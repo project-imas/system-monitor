90fba61dd0426cbf8cccfca8eda1198bf61da82d
Alice <alice.xy.ren@gmail.com>
2014-07-24 08:08:22 -0400

    Initial Commit

---
+++ ListInfo/ListInfo.h
@@ -0,0 +1,13 @@
+//
+//  ListInfo.h
+//  ListInfo
+//
+//  Created by Ren, Alice on 7/24/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ListInfo : NSObject
+
+@end
---
+++ ListInfo/ListInfo.m
@@ -0,0 +1,13 @@
+//
+//  ListInfo.m
+//  ListInfo
+//
+//  Created by Ren, Alice on 7/24/14.
+//
+//
+
+#import "ListInfo.h"
+
+@implementation ListInfo
+
+@end
